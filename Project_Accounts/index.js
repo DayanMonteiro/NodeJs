f0ba02886497295c94aaaca0442785e0fc9c2e20
DayanMonteiro <dmsmamb@gmail.com>
2023-11-14 16:31:33 -0300

    create account

--- Project_Accounts/index.js
+++ Project_Accounts/index.js
@@ -26,7 +26,15 @@ function operation() {
     .then((answer) => {
       const action = answer["action"];
 
-      console.log(action);
+      if (action === "Criar conta") {
+        createAccount();
+      }
     })
     .catch((err) => console.log(err));
+}
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir"));
 }
