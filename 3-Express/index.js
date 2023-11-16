189a00530ef41c8d80966c9ea19f8a2655acd6be
DayanMonteiro <dmsmamb@gmail.com>
2023-11-16 14:34:58 -0300

    reading data from a POST

--- 3-Express/index.js
+++ 3-Express/index.js
@@ -1,29 +1,49 @@
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
-// middleware
-// const checkAuth = function (req, res, next) {
-//   req.authStatus = false;
-
-//   if (req.authStatus) {
-//     console.log("Está, logado, pode continuar");
-//     next();
-//   } else {
-//     console.log("Não está logado, faça o login para continuar");
-//     next();
-//   }
-// };
-
-// app.use(checkAuth);
-
 const path = require("path");
+
 const basePath = path.join(__dirname, "templates");
 
+// read the body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+var checkAuth = function (req, res, next) {
+  req.authStatus = true;
+
+  if (req.authStatus) {
+    console.log("Está logado, pode continuar");
+    next();
+  } else {
+    console.log("Não está logado, faça o login para continuar!");
+  }
+};
+
+app.use(checkAuth);
+
+app.get("/users/add", (req, res) => {
+  res.sendFile(`${basePath}/userform.html`);
+});
+
+app.post("/users/save", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+
+  console.log(name);
+  console.log(age);
+});
+
 app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(`Estamos buscando pelo usuário: ${id}`);
+  console.log(`Carregando usuário: ${req.params.id}`);
+
   res.sendFile(`${basePath}/users.html`);
 });
 
@@ -32,5 +52,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
+  console.log(`App rodando na porta:${port}`);
 });
