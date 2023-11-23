075d44576f4bc8aa87a307d6999100cf865af1ca
DayanMonteiro <dmsmamb@gmail.com>
2023-11-23 15:09:39 -0300

    rescuing data

--- 10-Sequelize-NodeJs/index.js
+++ 10-Sequelize-NodeJs/index.js
@@ -19,8 +19,34 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.render("home");
+app.get("/users/create", (req, res) => {
+  res.render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  console.log("usuário", req.body);
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  console.log("users home", users);
+
+  res.render("home", { users: users });
 });
 
 conn
