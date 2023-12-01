e1ea0c71e6f6d19fe474ad67c6268200cc00274d
DayanMonteiro <dmsmamb@gmail.com>
2023-12-01 20:57:11 -0300

    mvc framework

--- 12-Nodejs-MongoDB/index.js
+++ 12-Nodejs-MongoDB/index.js
@@ -4,6 +4,8 @@ const exphb = require("express-handlebars");
 const app = express();
 const conn = require("./db/conn");
 
+const productsRoutes = require("./routes/productsRoutes");
+
 app.engine("handlebars", exphb());
 app.set("view engine", "handlebars");
 
@@ -14,5 +16,9 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/products", productsRoutes);
 
 app.listen(3000);
