061a7944615126119e6cebfb30f9803762c310bb
DayanMonteiro <dmsmamb@gmail.com>
2023-12-04 11:57:10 -0300

    remove products

--- 12-Nodejs-MongoDB/models/Product.js
+++ 12-Nodejs-MongoDB/models/Product.js
@@ -35,6 +35,15 @@ class Product {
 
     return product;
   }
+
+  static async removeProductById(id) {
+    await conn
+      .db()
+      .collection("products")
+      .deleteOne({ _id: ObjectId(id) });
+
+    return;
+  }
 }
 
 module.exports = Product;
