b47a08f85a8b4157cd7c5350c7eda5eca917a39b
DayanMonteiro <dmsmamb@gmail.com>
2023-12-05 14:04:59 -0300

    status api

--- 14-API/index.js
+++ 14-API/index.js
@@ -10,8 +10,24 @@ app.use(
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ message: "Primeira rota criada com sucesso!" });
+app.post("/createproduct", (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  if (!name) {
+    res.status(422).json({ message: "O campo nome é obrigatório" });
+    return;
+  }
+
+  console.log("nome: " + name, "o preço é: " + price);
+
+  res.status(201).json({
+    message: `O produto: ${name} custa ${price}, ele foi criado com sucesso!`,
+  });
 });
 
-app.listen(3000);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Primeira rota criada com sucesso!" });
+});
+
+app.listen(3001);
