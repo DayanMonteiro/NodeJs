a06fcf621dbb214af5a706d2a1574fd166ae030f
DayanMonteiro <dmsmamb@gmail.com>
2024-03-26 17:27:05 -0300

    parsing request bodies

--- 15-Course-The-Complete-Guide(MVC, REST, APIs, GraphQL, Deno)/02-Understanding-the-Basics/app.js
+++ 15-Course-The-Complete-Guide(MVC, REST, APIs, GraphQL, Deno)/02-Understanding-the-Basics/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<header><title>NodeJs Init</title></header>");
@@ -12,8 +13,17 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  if (url === "/" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk", chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
